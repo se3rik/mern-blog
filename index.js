4cfcf682a17fe45d3f9ea11a672898730c43725a
se3rik <se3rik@mail.ru>
2024-10-29 11:36:31 +0300

    feat: JWT & Mongoose integrations

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://se3rik:SeregaSwapp1232004@mern-cluster.dtimc.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster"
+  )
+  .then(() => {
+    console.log("DataBase OK");
+  })
+  .catch((err) => {
+    console.log(`DataBase Error`, err);
+  });
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Vasya Pupkin",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4444, (err) => {
