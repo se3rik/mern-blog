dafaea5cb86e927b6a6105003fe3b1048ef674b6
se3rik <se3rik@mail.ru>
2024-11-01 17:32:00 +0300

    feat: multer integration & uploading images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 
 import { registrationValidation, loginValidation } from "./validations/auth.js";
 import { postCreateValidation } from "./validations/post.js";
@@ -21,7 +22,19 @@ mongoose
   });
 
 const app = express();
+const storage = multer.diskStorage({
+  destination: (_, __, callback) => {
+    callback(null, "uploads");
+  },
+  filename: (_, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.post(
   "/auth/registration",
@@ -30,6 +43,12 @@ app.post(
 );
 app.post("/auth/login", loginValidation, UserController.authorization);
 app.get("/auth/me", checkAuth, UserController.getProfileInfo);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
